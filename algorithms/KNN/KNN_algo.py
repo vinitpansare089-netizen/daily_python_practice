a0545efea2285ac1b99c367a30955f4c3c3ecc94
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-07 23:35:49 +0530

    Scaling features for KNN

--- algorithms/KNN/KNN_algo.py
+++ algorithms/KNN/KNN_algo.py
@@ -4,11 +4,9 @@ import numpy as np
 x = np.array([[1],[2],[3],[6],[7],[8]])
 y = np.array([0,0,0,1,0,1])
 
-
 model = KNeighborsClassifier(n_neighbors=3)
-
 model.fit(x, y)
 
 pred = model.predict([[5]])
 
-print("prediction: ", pred)
+print("prediction: ", pred) 
