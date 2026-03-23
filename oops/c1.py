e1aed12a4b891091f05461d218254d7625238c4a
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-24 00:49:00 +0530

    try github copilot in c1

---
+++ oops/c1.py
@@ -0,0 +1,23 @@
+class calculator:
+    def home():
+        print("vinit")
+
+    def subtract(self, a, b):
+        return a - b
+
+    home()
+
+class calculator:
+    def home():
+        print("vinit")
+
+    def subtract(self, a, b):
+        return a - b
+
+    home()
+
+    #add calculator class
+    def add(self, a, b): 
+        return a + b
+    
+    class calculator:   
